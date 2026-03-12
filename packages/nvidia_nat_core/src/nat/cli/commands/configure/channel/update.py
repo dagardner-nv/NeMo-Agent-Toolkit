10ce0f60595dbb19cddb47088f2e4c00c2139c65
David Gardner <dagardner@nvidia.com>
2026-03-12 14:19:09 -0700

    Remove unused logger
    Signed-off-by: David Gardner <dagardner@nvidia.com>

--- packages/nvidia_nat_core/src/nat/cli/commands/configure/channel/update.py
+++ packages/nvidia_nat_core/src/nat/cli/commands/configure/channel/update.py
@@ -13,11 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-
 import click
-
-logger = logging.getLogger(__name__)
 
 
 @click.group(name="update",
